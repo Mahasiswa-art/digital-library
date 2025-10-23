8c46c07df64aa53e5e090b3de74d20c71c4d50bd
Mahasiswa-art <emailkamu@contoh.com>
2025-10-23 15:28:05 +0700

    Initial commit - add backend project files

---
+++ database/migrations/2025_10_23_054543_create_loans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+
+            // relasi ke tabel members dan books
+            $table->foreignId('member_id')->constrained('members')->onDelete('cascade');
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+
+            // kolom tanggal peminjaman dan pengembalian
+            $table->date('loan_date');
+            $table->date('return_date')->nullable();
+
+            // status pinjaman
+            $table->enum('status', ['borrowed', 'returned'])->default('borrowed');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('loans');
+    }
+};
